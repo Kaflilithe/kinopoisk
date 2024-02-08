9d6f32c5d52948e98b01d4c7653d98b68ae5905a
daraivanova <kudiyarova.1975@gmail.com>
2024-02-08 22:40:01 +0300

    компонент layout был добавлен на страницу

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { Layout } from '@/layout/Layout';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <Layout>
+      <main></main>
+    </Layout>
   );
 }
